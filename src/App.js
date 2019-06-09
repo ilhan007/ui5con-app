4a88dff1fdde55b6365f53cf9c83bd41ebbd2eaa
ilhan <ilhan.myumyun@sap.com>
2019-06-09 21:52:46 +0300

    add profile area

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import profile from "./img/profile.png";
 import logo from "./img/logo.png";
-import "@ui5/webcomponents/dist/Title";
 import "@ui5/webcomponents/dist/Card";
-import "@ui5/webcomponents/dist/List";
-import "@ui5/webcomponents/dist/StandardListItem";
-import "@ui5/webcomponents/dist/TabContainer";
-import "@ui5/webcomponents/dist/Tab";
+import "@ui5/webcomponents/dist/Icon";
 import "@ui5/webcomponents/dist/Input";
-import "@ui5/webcomponents/dist/DatePicker";
-import "@ui5/webcomponents/dist/Timeline";
+import "@ui5/webcomponents/dist/Label";
+import "@ui5/webcomponents/dist/List";
+import "@ui5/webcomponents/dist/CustomListItem";
+import "@ui5/webcomponents/dist/StandardListItem";
 import "@ui5/webcomponents/dist/ShellBar";
+import "@ui5/webcomponents/dist/Switch";
+import "@ui5/webcomponents/dist/Tab";
+import "@ui5/webcomponents/dist/TabContainer";
+import "@ui5/webcomponents/dist/Timeline";
+import "@ui5/webcomponents/dist/Title";
+import { setTheme } from "@ui5/webcomponents-base/Theming";
 
 import Navigation from "./navigation/Navigation";
 import Home from "./home/Home";
@@ -22,12 +26,7 @@ import Detail from './detail/Detail';
 const App = () => {
 	return (
 		<div className="App">
-			<ui5-shellbar
-				primary-title="Dashboard"
-				show-notifications notification-count="12"
-				profile={profile}
-				logo={logo}>
-			</ui5-shellbar>
+			<AppBar />
 
 			<Route path='/home' component={Navigation}/>
 
@@ -40,5 +39,79 @@ const App = () => {
 		</div>
 	);
 }
+
+class AppBar extends Component {
+
+	constructor (props) {
+		super(props);
+		this.appBar = React.createRef();
+		this.themeSwitch = React.createRef();
+	}
+
+	componentDidMount() {
+		this.appBar.current.addEventListener("profilePress", this.onProfilePressed);
+		this.themeSwitch.current.addEventListener("change", this.onThemeSwitchPressed.bind(this));
+	}
+
+	onProfilePressed(event) {
+		window["profile-popover"].openBy(event.detail.targetRef);
+	}
+
+	onThemeSwitchPressed(event) {
+		const checked = event.target.checked;
+		const appContainerDOMElement = this.getAppContainerDOM();
+		setTheme(checked ? "sap_belize_hcb" : "sap_fiori_3");
+
+		if (checked) {
+			appContainerDOMElement.classList.add("hcb");
+		} else {
+			appContainerDOMElement.classList.remove("hcb");
+		}
+	}
+
+	getAppContainerDOM() {
+		return document.querySelector(".App");
+	}
+
+	render() {
+		return (
+			<div>
+				<ui5-shellbar
+					ref={this.appBar}
+					primary-title="Smart Store Manager"
+					show-notifications
+					show-product-switch
+					profile={profile}
+					logo={logo}>
+				</ui5-shellbar>
+
+				<ui5-popover id="profile-popover" hide-header placement-type="Bottom" horizontal-align="Right">
+					<div className="profile-header">
+						<img src={profile} className="profile-img"/>
+						<ui5-title level="3">Darius Cummings</ui5-title>
+						<ui5-label>Store Manager</ui5-label>
+					</div>
+
+					<div className="profile-content">
+
+					<div className="profile-hcb">
+						<ui5-list>
+							<ui5-li icon="sap-icon://palette" type="Inactive">High Contrast Black</ui5-li>
+						</ui5-list>
+						<ui5-switch ref={this.themeSwitch}></ui5-switch>
+					</div>
+
+					<ui5-list separators="None">
+						<ui5-li icon="sap-icon://settings">Settings</ui5-li>
+						<ui5-li icon="sap-icon://sys-help">Help</ui5-li>
+						<ui5-li icon="sap-icon://log">Sign out</ui5-li>
+					</ui5-list>
+					</div>
+				</ui5-popover>
+			</div>
+		);
+	}
+}
+
 
 export default App;
--- src/home/Home.js
+++ src/home/Home.js
@@ -19,82 +19,90 @@ class Home extends Component {
 	render(){
 		return(
 			<div className="content">
-				{/* Input group */}
-				<section className="section">
-					<ui5-datepicker placeholder="start date" class="ui5datepicker"></ui5-datepicker>
-					<ui5-datepicker placeholder="end date"	 class="ui5datepicker"></ui5-datepicker>
-					<ui5-input id="searchInput" placeholder="Enter search criteria ..." class="ui5input">
-						<ui5-icon
-							id="searchIcon"
-							slot="icon"
-							src="sap-icon://search"
-							class="ui5inputIcon"></ui5-icon>
-					</ui5-input>
-				</section>
 
-				{/* Small Cards */}
-				<ui5-title>Overview</ui5-title>
+				<ui5-title>Featured</ui5-title>
 				<section className="section">
 					<ui5-card
 						ref={this.card}
-						heading="9,894"
-						subtitle="members online"
+						heading="Inventory"
+						subtitle="28 Jun 2019"
+						status="3 of 22"
 						class="ui5card">
+							<ui5-list separators="Inner">
+								<ui5-li icon="sap-icon://product" description="1579340" class="ui5list-item">Soap Bars</ui5-li>
+								<ui5-li icon="sap-icon://product" description="1579441" class="ui5list-item">Lettuce</ui5-li>
+								<ui5-li icon="sap-icon://product" description="1579542" class="ui5list-item">Broccoli</ui5-li>
+							</ui5-list>
 					</ui5-card>
 
 					<ui5-card
-						heading="154,786"
-						subtitle="Weekly visitors"
+						heading="Security"
+						subtitle="Cameras"
+						status="3 of 3"
 						class="ui5card">
-					</ui5-card>
-
-					<ui5-card
-						heading="1.4 M $"
-						subtitle="Q3 revenue"
-						class="ui5card">
+							<ui5-list separators="Inner">
+								<ui5-li icon="sap-icon://video" description="#36" class="ui5list-item">Parking A</ui5-li>
+								<ui5-li icon="sap-icon://video" description="#41" class="ui5list-item">Deli Department 1</ui5-li>
+								<ui5-li icon="sap-icon://video" description="#75" class="ui5list-item">Warehouse B</ui5-li>
+							</ui5-list>
 					</ui5-card>
 
 				</section>
 
-				{/* Large Cards */}
-				<ui5-title>Shedule</ui5-title>
+				<ui5-title>Today</ui5-title>
 				<section className="section">
+
+				<ui5-card
+					heading="Upcoming Activities"
+					subtitle="28 Jun 2019"
+					status="(3)"
+					class="ui5card">
 					<ui5-timeline>
-						<ui5-timeline-item id="test-item" title-text="called" subtitle-text="20.02.2017 11:30" icon="sap-icon://phone" item-name="John Smith" item-name-clickable></ui5-timeline-item>
-						<ui5-timeline-item title-text="Weekly Sync - CP Design" subtitle-text="27.07.2017 (11:00 - 12:30)" icon="sap-icon://calendar">
-							<div>MR SOF02 2.43</div>
+						<ui5-timeline-item title-text="Weekly Sync - Managers and Tech support" subtitle-text="10:00 - 10:30" icon="sap-icon://calendar">
+							<div>MR WDF18 C3(GLASSBOX)</div>
 						</ui5-timeline-item>
-						<ui5-timeline-item title-text="Video Converence Call - UI5" subtitle-text="31.01.2018 (12:00 - 13:00)" icon="sap-icon://calendar">
-							<div>Online meeting</div>
+						<ui5-timeline-item title-text="Video Converence Call - Tech Update" subtitle-text="14:30 - 15:30" icon="sap-icon://calendar">
+							<div>St. Leon Rot, NYC</div>
 						</ui5-timeline-item>
+						<ui5-timeline-item id="test-item" title-text="Call 'Project Nimbus' (Skype Meeting)" subtitle-text="15:00 - 16:30" icon="sap-icon://phone"></ui5-timeline-item>
 					</ui5-timeline>
+				</ui5-card>
 
 					<ui5-card
-						avatar="sap-icon://activities"
-						heading="TODO"
-						subtitle="12 Jul 2020"
-						status="6 of 6"
+						heading="Energy Efficiency"
+						subtitle="Smart Store A"
+						status="(4)"
 						class="ui5card">
-							<ui5-list separators="None" class="card-content-child">
-								<ui5-li description="Software Architect">Call Richard Wilson</ui5-li>
-								<ui5-li description="at NY Stock Exchange">Check Stocks market</ui5-li>
+							<ui5-list separators="Inner">
+								<ui5-li icon="sap-icon://fridge" description="Fridges are On" class="ui5list-item">Fridges</ui5-li>
+								<ui5-li icon="sap-icon://lightbulb" description="Light are On" class="ui5list-item">Lights</ui5-li>
+								<ui5-li icon="sap-icon://heating-cooling" description="All are On" class="ui5list-item">Air Condition</ui5-li>
+								<ui5-li icon="sap-icon://washing-machine" description="Ovens are On" class="ui5list-item">Air Condition</ui5-li>
 							</ui5-list>
 					</ui5-card>
 
 					<ui5-card
-						avatar="sap-icon://group"
-						heading="Team 6"
-						subtitle="Direct Reports"
+						avatar="sap-icon://retail-store"
+						heading="Smart Stores"
+						subtitle="Bulgaria"
 						status="6 of 6"
-						class="ui5card">
-							<ui5-list separators="None" class="card-content-child">
-								<ui5-li description="User Researcher">Alain Chevalier</ui5-li>
-								<ui5-li description="Artist">Monique Legrand</ui5-li>
-								<ui5-li description="UX Specialist">Michael Adams</ui5-li>
+						class="medium">
+						<div class="card-content">
+							<ui5-list separators="Inner" class="card-content-child">
+								<ui5-li description="New York City">Smart Store 1</ui5-li>
+								<ui5-li description="Washington">Smart Store 2</ui5-li>
+								<ui5-li description="Boston">Smart Store 3</ui5-li>
 							</ui5-list>
+							<ui5-list separators="Inner" class="card-content-child">
+								<ui5-li description="Los Angeles">Smart 4</ui5-li>
+								<ui5-li description="Dallas">Smart Store 5</ui5-li>
+								<ui5-li	 description="Chicago">Smart Store 6</ui5-li>
+							</ui5-list>
+						</div>
 					</ui5-card>
 				</section>
 
+				<ui5-title>Action Required</ui5-title>
 				<section className="section">
 				</section>
 			</div>
